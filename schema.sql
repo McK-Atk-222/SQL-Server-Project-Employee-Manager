d64b20c831347eb8d99d9ed041593ab637e1fe13
McK-Atk-222 <mckay.atkinson@gmail.com>
2025-02-06 20:15:45 -0700

    Completed schema.sql

--- schema.sql
+++ schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE roles (
   id SERIAL PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES departments(id)
   ON DELETE SET NULL
@@ -21,10 +22,12 @@ CREATE TABLE employees (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
   FOREIGN KEY (role_id)
-  REFERENCES roles(id)
-  -- FOREIGN KEY (manager_id)
-  -- REFERENCES managers(id)
+  REFERENCES roles(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(id)
   ON DELETE SET NULL
 );
 
