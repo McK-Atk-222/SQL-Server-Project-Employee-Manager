292a3720282a8b77a3b13ea8f263e47179b59065
McK-Atk-222 <mckay.atkinson@gmail.com>
2025-02-22 11:51:06 -0700

    updated functions in index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const inquirer = require('inquirer');
+import inquirer from 'inquirer';
+import {connectToDb} from './connections.js';
+
+const client = await connectToDb();
 
 function mainMenu() {
     inquirer.prompt([
@@ -24,25 +27,42 @@ function mainMenu() {
                     const res = await client.query('SELECT * FROM departments');
                     console.table(res.rows);
                 }
+                viewAllDepartments();
+
                 break;
+
             case 'View all roles':
                 async function viewAllRoles() {
                     const res = await client.query('SELECT * FROM roles');
                     console.table(res.rows);
                 }
+                viewAllRoles();
+
                 break;
+
             case 'View all employees':
                 async function viewAllEmployees() {
                     const res = await client.query('SELECT * FROM employees');
                     console.table(res.rows);
                 }
+                viewAllEmployees();
+
                 break;
+
             case 'Add a department':
-                async function addDepartment(departmentName) {
-                    const res = await client.query('INSERT INTO department (name) VALUES ($1)', [departmentName]);
-                    console.log(`Department ${departmentName} added successfully.`);
+                async function addDepartment() {
+                    const name  = await inquirer.prompt({
+                        type: 'input',
+                        name: 'name',
+                        message: 'Enter department name:'
+                    });
+                    const res = await client.query('INSERT INTO departments (name) VALUES ($1)', [name]);
+                    console.log(`Department ${name} added successfully.`);
                 }
+                addDepartment();
+
                 break;
+
             case 'Add a role':
                 async function addRole() {
                     const { title } = await inquirer.prompt({
@@ -50,7 +70,7 @@ function mainMenu() {
                         name: 'title',
                         message: 'Enter the title of the role:'
                     });
-                
+
                     const { salary } = await inquirer.prompt({
                         type: 'input',
                         name: 'salary',
@@ -60,25 +80,108 @@ function mainMenu() {
                             return isNumber || 'Please enter a valid number';
                         }
                     });
-                
+
+                    async function viewAllDepartments() {
+                        const res = await client.query('SELECT * FROM departments');
+                        return res.rows;
+                    }
+
+                    const departmentName = viewAllDepartments()
+
                     const { department } = await inquirer.prompt({
                         type: 'list',
                         name: 'departmentId',
                         message: 'Which department is this role in?',
-                        choices: ${departmentName}
-                        }
+                        choices: departmentName, 
                     });
                 
-                    // Call the addRole function with the gathered input
-                    await addRole (title, salary, department);
-                }
+                const res = await client.query('INSERT INTO roles (title, salary, department_id) VALUES ($1,$2,$3)',[title,salary,department]);
+                    console.table(res.rows);
+
+                };
+                addRole(); 
+
                 break;
+                
             case 'Add an employee':
-                // function to add an employee
+                async function addEmployee() {
+                    const { firstName } = await inquirer.prompt({
+                        type: 'input',
+                        name: 'firstName',
+                        message: 'Enter employees first name:'
+                    });
+
+                    const { lastName } = await inquirer.prompt({
+                        type: 'input',
+                        name: 'lastName',
+                        message: 'Enter employees last name:',
+                    });
+
+                    async function viewAllroles() {
+                        const res = await client.query('SELECT * FROM roles');
+                        return res.rows;
+                    }
+
+                    const roleName = viewAllroles()
+
+                    const { role } = await inquirer.prompt({
+                        type: 'list',
+                        name: 'roleId',
+                        message: 'Which role is this employee in?',
+                        choices: roleName, 
+                    });
+
+                    const { manager } = await inquirer.prompt({
+                        type: 'input',
+                        name: 'manager',
+                        message: 'Enter employees manager:'
+                    });
+                
+                const res = await client.query('INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ($1,$2,$3,$4)',[firstName,lastName,role,manager]);
+                    console.table(res.rows);
+
+                };
+                addEmployee(); 
+
                 break;
-            case 'Update an employee role':
-                // function to update employee role
+
+                case 'Update an employee':
+                async function updateEmployee() {
+                    async function viewAllEmployees() {
+                    const res = await client.query('SELECT * FROM employees');
+                    return res.rows;
+                }
+
+                const employeeName = viewAllEmployees()
+
+                const { department } = await inquirer.prompt({
+                    type: 'list',
+                    name: 'employeeId',
+                    message: 'Select an employee to update:',
+                    choices: employeeName, 
+                });
+                async function viewAllroles() {
+                    const res = await client.query('SELECT * FROM roles');
+                    return res.rows;
+                }
+
+                const roleName = viewAllroles()
+
+                const { role } = await inquirer.prompt({
+                    type: 'list',
+                    name: 'roleId',
+                    message: 'Select updated role:',
+                    choices: roleName, 
+                });
+
+                const res = await client.query('INSERT INTO employees (role_id) VALUES ($1)',[role]);
+                console.table(res.rows);
+
+                };
+                updateEmployee(); 
+
                 break;
+
             case 'Exit':
                 console.log('Exiting...');
                 process.exit();
