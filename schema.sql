db5752df1509a4064f0e373466ca63a43d3360e4
McK-Atk-222 <mckay.atkinson@gmail.com>
2025-02-22 11:05:10 -0700

    Updated and completed schema.sql

--- schema.sql
+++ schema.sql
@@ -5,14 +5,14 @@ CREATE DATABASE business_db;
 
 CREATE TABLE departments (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE roles (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
   FOREIGN KEY (department_id)
   REFERENCES departments(id)
   ON DELETE SET NULL
@@ -20,10 +20,10 @@ CREATE TABLE roles (
 
 CREATE TABLE employees (
   id SERIAL PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
   FOREIGN KEY (role_id)
   REFERENCES roles(id),
   FOREIGN KEY (manager_id)
